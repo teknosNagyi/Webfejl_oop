d0fc249a8fcc9cc4676fd6a890351faa2f12d7f2
Gal Alex <gal.alex@BOLYAI.LOCAL>
2024-11-29 12:56:23 +0100

    megolommagam3

--- basics/main.js
+++ basics/main.js
@@ -1,16 +1,42 @@
-function Player(nickname){
-   this.nickname = nickname
-    this.playedmatch=0
+// function Player(nickname){
+//    this.nickname = nickname
+//     this.playedmatch=0
 
     
-};
+// };
 
-Player.prototype.play =  function(){
-    this.playedmatch++,
+// Player.prototype.play =  function(){
+//     this.playedmatch++,
+//     console.log(this.nickname,this.playedmatch)
+
+// }
+// Player.prototype.getTierlvl = function(){
+//     if (this.playedmatch<4){
+//         return "A tier"
+//     }
+//     else if (3<this.playedmatch && this.playedmatch<7){
+//         return "B tier"
+//     }
+//     else {
+//         return "C tiert"
+//     }
+        
+
+// }
+
+
+class Player{
+    constructor(nickname){
+        this.nickname
+        this.playedmatch = 0
+    }
+    play(){
+        this.playedmatch++,
     console.log(this.nickname,this.playedmatch)
-
-}
-Player.prototype.getTierlvl = function(){
+    }
+    getTierlvl(){
+    
+    
     if (this.playedmatch<4){
         return "A tier"
     }
@@ -20,9 +46,8 @@ Player.prototype.getTierlvl = function(){
     else {
         return "C tiert"
     }
-        
-
 }
+    }
 
 const gomszab = new Player("gomszab")
 gomszab.play()
@@ -31,4 +56,3 @@ gomszab.play()
 gomszab.play()
 console.log(gomszab.getTierlvl())
 console.log(gomszab)
-
