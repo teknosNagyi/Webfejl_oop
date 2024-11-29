7f237f445e363ead2e551df973921e03aa5d1308
Gal Alex <gal.alex@BOLYAI.LOCAL>
2024-11-29 12:48:19 +0100

    megolommagam2

---
+++ basics/main.js
@@ -0,0 +1,34 @@
+function Player(nickname){
+   this.nickname = nickname
+    this.playedmatch=0
+
+    
+};
+
+Player.prototype.play =  function(){
+    this.playedmatch++,
+    console.log(this.nickname,this.playedmatch)
+
+}
+Player.prototype.getTierlvl = function(){
+    if (this.playedmatch<4){
+        return "A tier"
+    }
+    else if (3<this.playedmatch && this.playedmatch<7){
+        return "B tier"
+    }
+    else {
+        return "C tiert"
+    }
+        
+
+}
+
+const gomszab = new Player("gomszab")
+gomszab.play()
+gomszab.play()
+gomszab.play()
+gomszab.play()
+console.log(gomszab.getTierlvl())
+console.log(gomszab)
+
